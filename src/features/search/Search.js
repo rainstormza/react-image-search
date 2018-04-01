5daa520b994ad0b9dfa1861fd410bdbfde2142dc
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-04-01 17:27:46 +0700

    + handle data with axios in reducer

--- src/features/search/Search.js
+++ src/features/search/Search.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Input, Layout, Row, Col } from 'antd'
+import { Input, Layout, Row, Col, Carousel } from 'antd'
 import { searchImage } from './redux'
+import './Search.css'
 
 const enhance = connect(state => state, { searchImage })
 const SearchAntD = Input.Search
 const { Header, Footer, Sider, Content } = Layout
 
 const Search = props => {
-  // console.log(props)
+  console.log(props)
+  const { searchImage, isLoading, data } = props
   return (
     <div>
       {' '}
@@ -18,12 +20,31 @@ const Search = props => {
           <Sider>Sider</Sider>
           <Row>
             <Col span={12} offset={6}>
+              <p>{isLoading ? 'loading' : 'not loading'}</p>
               <SearchAntD
                 placeholder="input search text"
-                onChange={e => props.searchImage(e.target.value)}
-                onSearch={value => props.searchImage(value)}
+                // onChange={e => props.searchImage(e.target.value)}
+                onSearch={value => {
+                  if (value) {
+                    return searchImage(value)
+                  }
+                  return
+                }}
                 enterButton
               />
+              {data.length ? (
+                <Carousel autoplay>
+                  {data.map(item => {
+                    return (
+                      <div key={item.id}>
+                        <img src={item.urls.small} alt="" />
+                      </div>
+                    )
+                  })}
+                </Carousel>
+              ) : (
+                <p>No image</p>
+              )}
             </Col>
           </Row>
         </Content>
--- src/features/search/redux.js
+++ src/features/search/redux.js
@@ -1,7 +1,15 @@
+import axios from 'axios'
+import config from '../../common/config'
+
 const SEARCH_IMAGE = 'SEARCH_IMAGE'
+const SEARCH_IMAGE_PENDING = 'SEARCH_IMAGE_PENDING'
+const SEARCH_IMAGE_FULFILLED = 'SEARCH_IMAGE_FULFILLED'
+const SEARCH_IMAGE_REJECTED = 'SEARCH_IMAGE_REJECTED'
 
 const initialState = {
-  search: ''
+  isLoading: false,
+  search: '',
+  data: []
 }
 
 export default (state = initialState, action) => {
@@ -12,14 +20,39 @@ export default (state = initialState, action) => {
         ...state,
         search: action.search
       }
-
+    case SEARCH_IMAGE_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case SEARCH_IMAGE_FULFILLED:
+      return {
+        ...state,
+        isLoading: false,
+        search: '',
+        data: action.payload.results
+      }
+    case SEARCH_IMAGE_REJECTED:
+      return {
+        ...state,
+        isLoading: false,
+        search: '',
+        data: []
+      }
     default:
       return state
   }
 }
 
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com',
+  headers: { Authorization: `Client-ID ${config.accessKey}` }
+})
+
 // action creator
 export const searchImage = search => ({
   type: SEARCH_IMAGE,
-  search
+  payload: instance.get(`/search/photos?page=1&query=${search}`).then(res => {
+    return res.data
+  })
 })
